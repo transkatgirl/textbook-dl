d8ae5d507da26b627919dbd4f52d38353b377bc6
Kat <10361870+transkatgirl@users.noreply.github.com>
2024-08-28 02:55:15 -0700

    Added popup dismissal code

--- src/openstax.ts
+++ src/openstax.ts
@@ -1,4 +1,4 @@
-import { Builder, Browser, By } from "selenium-webdriver";
+import { Builder, Browser, By, WebDriver } from "selenium-webdriver";
 import { Options } from "selenium-webdriver/chrome";
 
 export async function download(address: URL) {
@@ -8,18 +8,69 @@ export async function download(address: URL) {
 		.forBrowser(Browser.CHROME)
 		.setChromeOptions(options)
 		.build();
+	await driver.manage().setTimeouts({ implicit: 3000 });
 
 	await driver.get(address.href);
 
+	await cleanPage(driver);
+
 	console.log("Attempting to find table of contents...");
 	const toc = await driver.findElement(By.css('nav[data-testid="toc"] > ol'));
+
+	const tocItems = await toc.findElements(By.css('li[data-type="page"] > a'));
+
+	console.log(tocItems.length);
+
+	/*const tocItems = await driver.findElements(
+		By.css('li[data-type="chapter"] > details')
+	);*/
+
+	/*const tocItems = await toc.findElements(By.xpath("./child::*"));
+
+	for (const item of tocItems) {
+		await item.click();
+
+		console.log("Waiting 5 seconds for page to load...");
+		await new Promise((resolve) => setTimeout(resolve, 5000));
+	}*/
 
 	console.log("Attempting to find page content...");
 	const main = await driver.findElement(
 		By.css('#main-content > [data-type="page"]')
 	);
 
-	//const contentHTML = await revealed.getAttribute("innerHTML");
+	//const contentHTML = await main.getAttribute("innerHTML");
+}
 
-	//console.log(await revealed.getAttribute("innerHTML"));
+async function cleanPage(driver: WebDriver) {
+	console.log("Waiting 5 seconds for page to load...");
+	await new Promise((resolve) => setTimeout(resolve, 5000));
+
+	await driver.manage().setTimeouts({ implicit: 500 });
+
+	console.log("Attempting to dismiss any pop-ups...");
+
+	const buttons_1 = await driver.findElements(
+		By.css(".osano-cm-window__dialog button.osano-cm-accept")
+	);
+
+	const buttons_2 = await driver.findElements(
+		By.css(
+			'[data-analytics-region="Nudge Study Tools"] button[aria-label="close overlay"]'
+		)
+	);
+
+	const buttons_3 = await driver.findElements(
+		By.css("#_pi_surveyWidget ._pi_closeButton")
+	);
+
+	const buttons = buttons_1.concat(buttons_2).concat(buttons_3);
+
+	for (const button of buttons) {
+		await button.click();
+
+		await new Promise((resolve) => setTimeout(resolve, 2000));
+	}
+
+	await driver.manage().setTimeouts({ implicit: 3000 });
 }
