cd403161abce3aa3a444b6eab93b6281bba26f68
Kat <10361870+transkatgirl@users.noreply.github.com>
2024-09-02 16:32:36 -0700

    Started work on ebook building

--- src/builder.ts
+++ src/builder.ts
@@ -1,3 +1,5 @@
+import { DOMParser, MIME_TYPE, XMLSerializer } from "@xmldom/xmldom";
+
 export interface RawTextbook {
 	meta: RawTextbookMetadata;
 	nav: RawNavItem[];
@@ -18,4 +20,13 @@ export interface RawNavItem {
 	subitems: RawNavItem[];
 }
 
-export function buildTextbook(input: RawTextbook) {}
+export function buildTextbook(input: RawTextbook) {
+	for (const [filename, contents] of input.pages.entries()) {
+		const document = new DOMParser().parseFromString(
+			"<!DOCTYPE html><body>" + contents + "</body>",
+			MIME_TYPE.HTML
+		);
+
+		const serialized = new XMLSerializer().serializeToString(document);
+	}
+}
