afcbecf681f42689ecda5515e9405d05b9b5be30
Kat <10361870+transkatgirl@users.noreply.github.com>
2024-09-03 06:46:36 -0700

    Code cleanup

--- src/builder.ts
+++ src/builder.ts
@@ -233,9 +233,9 @@ function buildNav(lang: string, nav: RawNavItem[]): string {
 	console.log("Building EPUB navigation document...");
 
 	const dom = new JSDOM(
-		'<?xml version="1.0" encoding="utf-8"?><!DOCTYPE html><html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops" lang="' +
+		'<!DOCTYPE html><html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops" lang="' +
 			lang +
-			'"><head><title>Book Navigation</title><meta charset="utf-8"/></head><body epub:type="frontmatter"><nav epub:type="toc" id="toc" role="doc-toc"><h1>Table of Contents</h1></nav><nav epub:type="landmarks" id="landmarks" hidden=""><h2>Landmarks</h2><ol><li><a epub:type="toc" href="#toc">Table of Contents</a></li></ol></nav></body></html>',
+			'"><head><meta charset="utf-8"/><title>Book Navigation</title></head><body epub:type="frontmatter"><nav epub:type="toc" id="toc" role="doc-toc"><h1>Table of Contents</h1></nav><nav epub:type="landmarks" id="landmarks" hidden=""><h2>Landmarks</h2><ol><li><a epub:type="toc" href="#toc">Table of Contents</a></li></ol></nav></body></html>',
 		{ contentType: "application/xhtml+xml" }
 	);
 	const document = dom.window.document;
